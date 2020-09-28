0e63c3946a86d62a272e55a98f472c13df6f88b9
bolicoding <bolicoding@gmail.com>
2020-09-28 15:51:02 -0600

    get all guides route

--- app.py
+++ app.py
@@ -30,11 +30,22 @@ guides_schema = GuideSchema(many=True)
 def add_guide():
   title = request.json['title']
   content = request.json['content']
+  
   new_guide = Guide(title, content)
+  
   db.session.add(new_guide)
   db.session.commit()
+  
   guide = Guide.query.get(new_guide.id)
+  
   return guide_schema.jsonify(guide)
+
+# endpoint to query all guides
+@app.route('/guides', methods=["GET"])
+def get_guides():
+  all_guides = Guide.query.all()
+  result = guides_schema.dump(all_guides)
+  return jsonify(result)
 
 if __name__ == '__main__':
   app.run(debug=True)
