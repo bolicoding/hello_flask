7eed3dd6d74343f3a9c752a4b8ca48723222b62a
bolicoding <bolicoding@gmail.com>
2020-09-28 16:01:31 -0600

    get single guide by id route

--- app.py
+++ app.py
@@ -47,5 +47,13 @@ def get_guides():
   result = guides_schema.dump(all_guides)
   return jsonify(result)
 
+
+#Endpoint for querying single guide
+@app.route("/guide/<id>", methods=["GET"])
+def get_guide(id):
+  guide = Guide.query.get(id)
+  return guide_schema.jsonify(guide)
+
+
 if __name__ == '__main__':
   app.run(debug=True)
